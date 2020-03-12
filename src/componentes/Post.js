c0155461d12aa53c2239fff93117c23082c77a87
fernandagadelha <fegadelhasouza@gmail.com>
2020-03-12 16:44:12 -0300

    Atualização de arquivos

---
+++ src/componentes/Post.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Post.css';
+import Imagem from './mulher.png';
+
+class Post extends React.Component {
+    render() {
+        return (
+            <div className="post">
+                
+                <img src={Imagem} alt="imagem"></img>
+                <div className="usuarioAutor">Fernanda</div>
+                <div className="legenda">Demorei um tempinho nessa aqui. Espero que gostem!</div>
+                
+                
+            </div>
+
+        )
+    }
+
+}
+
+export default Post;
